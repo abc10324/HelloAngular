da5b9899fc9bdc31ec819f790f61f10a7e01d371
Sam Kao <abc10324@hotmail.com>
2020-02-29 11:52:37 +0800

    Add login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { MRTstationComponent } from './mrtstation/mrtstation.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  entryComponents:[ MRTstationComponent,UserLoginComponent ]
 })
 export class AppComponent {
+  mapping = new Map<string,any>([
+    ['MRT',MRTstationComponent],
+    ['Login',UserLoginComponent]
+  ]);
+  
+
   currentModule = '';
 
-  changeModule(moduleName : string){
+  changeModuleTo(moduleName : string){
     this.currentModule = moduleName;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MRTstationComponent } from './mrtstation/mrtstation.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MRTstationComponent
+    MRTstationComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
